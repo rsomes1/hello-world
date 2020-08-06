18d12a676009188ee5818c9d41a84849997f12c5
rsomes1 <44736508+rsomes1@users.noreply.github.com>
2020-08-06 09:14:20 -0700

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Hello.c
+ Author      : RKS
+ Version     :
+ Copyright   : Public domain
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void) {
+
+	printf("!!!Hello World.  Are you still there RKS???"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
